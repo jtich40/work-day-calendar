b0c34d3723ce0795edb3649f37ac8b6bc4b16600
Jared Tichacek <jared.tichacek@gmail.com>
2023-01-14 21:12:44 -0600

    added fix to ensure booked appointments persist after page refresh

--- script.js
+++ script.js
@@ -54,7 +54,16 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'))
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'))
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'))
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'))
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'))
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'))
   // TODO: Add code to display the current date in the header of the page.
   let today = dayjs().format('dddd, MMMM D, YYYY');
   $('#currentDay').text(today)
